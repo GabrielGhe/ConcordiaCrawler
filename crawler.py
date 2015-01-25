ea4de41dadbf127d32b2e733e21695217c9f005e
Gabriel Gheorghian <gabriel.gheorghian@gmail.com>
2015-01-24 17:29:40 -0800

    added ratemyprofessor

--- crawler.py
+++ crawler.py
@@ -10,7 +10,24 @@ def crawl():
     # get all the links
     for link in soup.findAll('a'):
         link_href = link.get('href')
-        if link_href.startswith('details'):
-            print( "{} {}".format(link.text, link_href) )
 
+        # only if the link is for a class do we display it
+        if link_href.startswith('details'):
+            link_href = "http://aits.encs.concordia.ca/oldsite/resources/schedules/courses/" + link_href
+            print( "{} {}".format(link.text, link_href) )
 crawl()
+
+
+def ratemyprof():
+    # used to find the professors at concordia with the prefix
+    ratemyprofessor = ("http://search.mtvnservices.com/typeahead/suggest/?solrformat=true"
+                       "&rows=10&callback=noCB&prefix=")
+    ratemyprofessor += "aiman"
+    ratemyprofessor += ("&qf=teacherfullname_t%5E1000+teacherfullname_autosuggest"
+                        "&bf=pow(total_number_of_ratings_i%2C2.1)&defType=edismax&siteName=rmp"
+                        "&group=off&group.field=content_type_s&group.limit=20"
+                        "&fq=content_type_s%3ATEACHER&fq=schoolid_s%3A1422"
+                        "&fq=schoolname_t%3A%22Concordia+University%22&fq=schoolid_s%3A1422")
+
+    # go to the professor's page
+    prof_page = "http://www.ratemyprofessors.com/ShowRatings.jsp?tid="
